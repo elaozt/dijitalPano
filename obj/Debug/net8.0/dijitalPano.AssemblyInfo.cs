7f6f1efe1479e15c1a7bf7f22acff4d6ec505eca
ela öztürk git config --global user.name  git config --global user.name ela <elaozturk767@gmail.com>
2024-05-21 19:23:36 +0300

    header

--- obj/Debug/net8.0/dijitalPano.AssemblyInfo.cs
+++ obj/Debug/net8.0/dijitalPano.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("dijitalPano")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6639090cbf1cf9445dc81b46652381e8adf6f322")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cc5bc88d589c3997a58b995d9d1b581405b3ba22")]
 [assembly: System.Reflection.AssemblyProductAttribute("dijitalPano")]
 [assembly: System.Reflection.AssemblyTitleAttribute("dijitalPano")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
